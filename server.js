1a267c393a8c93a8ee3646a5169013b125cd81dd
mithlesh prasad <101449591+mithleshprasad@users.noreply.github.com>
2024-11-29 16:55:21 +0000

    Array Start

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
-console.log("welcome to dsa start")
+const express = require("express");
+
+const app = express();
+const PORT = 8045;
+app.use(express.json());
+app.get('/',(req,res) =>{
+    res.send("Welcome to the DSA server! 🚀")
+})
+app.get("/info",(req,res) =>{
+    res.json({
+        status: true,
+        message:"json server run successfull!",
+        server:express
+    })
+})
+app.get("/array",(req,res)=>{
+    const arr= [1,2,3,4,5,6];
+    const reveresArr = arr.reverse();
+    res.json({
+        status:true,
+        array: `Originally array ${[1,2,3,4,5,6]} <br> array :${reveresArr}`
+    })
+})
+// Handle 404 (Not Found) for any undefined routes
+
+app.use((req,res)=>{
+res.status("oops ! Route not found.")
+})
+app.listen(PORT ,() => {
+    console.log(`Server running on port ${PORT}. Access it at http://localhost:${PORT}`);
+})
+
+
+
